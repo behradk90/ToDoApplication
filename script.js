ef3ee42d765e489059365ae7dd768a9adcdcf6f5
behradk90 <83545119+behradk90@users.noreply.github.com>
2021-07-05 21:12:22 +0100

    Basic styles added using bootstrap
    Logic optimised to encapsulate both a click event and a keypress event.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-
 const input = document.querySelector('#input');
 const sButton = document.querySelector('.submit');
 
-sButton.addEventListener("click", () => {
+
+sButton.addEventListener("click", eventHandle);
+input.addEventListener("keypress", function onEvent(e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        eventHandle()
+    }
+
+});
+
+function eventHandle() {
     const toDoList = document.querySelector('.toDoList');
     const list = document.createElement('li');
+    const btn = document.createElement("button");
+    list.className = "list-group-item"
+    btn.className = "btn btn-danger mx-2"
+    btn.addEventListener("click", () => {
+        list.parentNode.removeChild(list);
+    });
+    btn.innerText = "Remove";
     list.appendChild(document.createTextNode(input.value));
+    list.appendChild(btn)
     toDoList.appendChild(list);
-})
-
-console.log(input)
+    input.value = '';
+}
